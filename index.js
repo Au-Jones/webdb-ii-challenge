9b3907be9c45c37f454815461f0661e508b6cf8e
Your Name <aujones95@gmail.com>
2019-06-18 14:41:55 -0400

    finished the server.get

--- index.js
+++ index.js
@@ -18,7 +18,38 @@ server.use(helmet());
 
 // endpoints here
 
+server.get('/api/zoos', (req, res) => {
+  db('zoos')
+  .then(zoos => {
+    res.status(200).json(zoos)
+  })
+  .catch(error => {
+    res.status(400).json({
+      message: 'could not get zoos'
+    })
+  })
+})
 
+server.get('/api/zoos/:id', (req, res) => {
+  db('zoos')
+  .where({ id: req.params.id})
+  .then(zoo => {
+    if(zoo) {
+      res.status(200).json(zoo)
+    }else{
+      res.status(404).json({
+        message: 'zoo not found'
+      })
+    }
+  })
+  .catch(error => {
+    res.status(500).json(error)
+  }) 
+})
+
+server.post('/api/zoos', (req, res) => {
+  const
+})
 
 const port = 3300;
 server.listen(port, function() {
