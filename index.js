fb273e7c05e3c8c3c7e0d2333955cb7bec6909a5
Your Name <aujones95@gmail.com>
2019-06-18 14:29:09 -0400

    added the knex in the index

--- index.js
+++ index.js
@@ -1,12 +1,24 @@
 const express = require('express');
 const helmet = require('helmet');
-
+const knex = require('knex')
 const server = express();
+
+const knexconfig = {
+  client:'sqlite3',
+  connection: {
+    filename: '/data/lambda.sqlite3'
+  },
+  useNullAsDefault: true
+}
+
+const db = knex(knexconfig)
 
 server.use(express.json());
 server.use(helmet());
 
 // endpoints here
+
+
 
 const port = 3300;
 server.listen(port, function() {
